6ee8c768536ada94eed6c512b1558230091e4f17
god-lamp <2545977650@qq.com>
2024-11-19 23:44:09 +0800

    init:初始化项目

---
+++ src/main/java/com/phoenix/devops/PhoenixOpsApplication.java
@@ -0,0 +1,17 @@
+package com.phoenix.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@EnableWebSocket
+@EnableScheduling
+@EnableWebSecurity
+@SpringBootApplication
+public class PhoenixOpsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixOpsApplication.class, args);
+    }
+}
